fabc7a4e9c88b5a1a69251c94fd7103addf99af1
zajanorbert <zaja.norbert@gmail.com>
2019-01-28 14:32:08 +0100

    xmlParse

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -2,9 +2,12 @@ package com.codecool;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         FactParser fparser = new FactParser();
         RuleParser rparser = new RuleParser();
         ESProvider exsys = new ESProvider(fparser, rparser);
+        fparser.loadXmlDocument("facts.xml");
+        rparser.loadXmlDocument("rules.xml");
     }
+
 }
